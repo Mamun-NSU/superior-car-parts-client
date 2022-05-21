3dce95b5091349b4eaad8050ed12519988efd766
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-05-21 23:55:28 +0600

    Navber created successfully

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
+
 
 function App() {
   return (
-    <h2>Hello! I'm ready.</h2>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </div>
+
   );
 }
 
