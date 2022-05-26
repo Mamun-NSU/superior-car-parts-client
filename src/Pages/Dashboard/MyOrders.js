1211262b6f0d02898a4947b11512c74432751ef1
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-05-26 08:54:43 +0600

    My Order page updated successfully

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,80 @@
-import React from "react";
+import { signOut } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const MyOrders = () => {
+  const [orders, setOrders] = useState([]);
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate()
+
+
+  // pw: m.P$@-Y7+K23V?h
+  //  use:882
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/orders?user_email=${user.email}`, {
+        method: 'GET',
+        headers: {
+          'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+      })
+        .then(res => {
+          console.log('res', res);
+          if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem('accessToken');
+            navigate('/');
+          }
+          return res.json()
+        })
+        .then(data => {
+
+          setOrders(data);
+        });
+    }
+  }, [user, navigate])
   return (
+
     <div>
-      <h1>This is MyOrders page</h1>
+      <h2>Orders for: {user.displayName}</h2>
+      <h2>My Orders: {orders.length}</h2>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Product Name</th>
+              <th>Email</th>
+              <th>Quantity</th>
+              <th>Total Price</th>
+              <th>Payment</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              orders.map((order, index) => <tr key={order._id}>
+                <th>{index + 1}</th>
+                <td>{order.order_name}</td>
+                <td>{order.user_email}</td>
+                <td>{order.order_quantity}</td>
+                <td>{order.order_price}</td>
+                <td>
+                  {(order.order_price && !order.paid) && <Link to={`/dashboard/payment/${order._id}`}><button className='btn btn-xs btn-success'>pay</button></Link>}
+                  {(order.order_price && order.paid) && <div>
+                    <p><span className='text-success'>Paid</span></p>
+                    <p>Transaction id: <span className='text-success'>{order.transactionId}</span></p>
+                  </div>}
+                </td>
+              </tr>)
+            }
+
+
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
